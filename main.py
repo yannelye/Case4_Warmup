eb8cc15407626ae8c309ecd570cd5f424753cf9e
YannelyAreli_EscalanteRuiz <yannely2024@gmail.com>
2025-10-01 20:59:24 -0400

    Fix FastAPI port to 5000 for autograder

--- main.py
+++ main.py
@@ -49,3 +49,7 @@ def submit(data: UserSubmission):
         f.write(json.dumps(record) + "\n")
     
     return {"status": "success", "submission_id": record["submission_id"]}
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
+
